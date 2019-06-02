027578cd213817b8c6dc8749963b330c58d90396
Pawel Wodyk <pawlosik@yahoo.com>
2019-06-02 10:47:26 +0000

    new functionality add record

--- mongo_project.py
+++ mongo_project.py
@@ -12,7 +12,7 @@ def mongo_connect(url):
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
-
+        
 
 def show_menu():
     print("")
@@ -26,18 +26,45 @@ def show_menu():
     return option
 
 
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_colour = input("Enter hair colour > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+
+    new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob,
+               'gender': gender, 'hair_colour': hair_colour, 'occupation':
+               occupation, 'nationality': nationality}
+    
+    try:
+        coll.insert(new_doc)
+        print("")
+        print("Document inserted")
+    except:
+        print("Error accessing the database")
+
+
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You have selected option 1")
+            #ADD
+            add_record()
         elif option == "2":
+            #FIND
             print("You have selected option 2")
         elif option == "3":
+            #EDIT
             print("You have selected option 3")
         elif option == "4":
+            #REMOVE
             print("You have selected option 4")
         elif option == "5":
+            #EXIT
             conn.close()
             break
         else:
@@ -49,4 +76,3 @@ conn = mongo_connect(MONGODB_URI)
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
 main_loop()
-
